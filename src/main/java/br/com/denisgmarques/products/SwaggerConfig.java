78a5bf4d03dd2daaf60e8b83c45551427a6e021d
Denis Giovan Marques <denis.marques@icolabora.com.br>
2022-11-11 11:41:41 -0300

    Package adjustment.

--- src/main/java/br/com/denisgmarques/products/SwaggerConfig.java
+++ src/main/java/br/com/denisgmarques/products/SwaggerConfig.java
@@ -11,7 +11,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootConfiguration
-@ComponentScan("br.com.acme.products.controller")
+@ComponentScan("br.com.denisgmarques.products.controller")
 @EnableSwagger2
 public class SwaggerConfig {
 
